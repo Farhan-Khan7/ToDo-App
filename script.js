6f90d9093121385d128139cb51603088b4a65673
Farhan-Khan7 <fk5518744@gmail.com>
2026-06-19 01:02:01 +0530

    I have theme button

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+const themebtn = document.querySelector(".dark-light-toggle");
+const lightModeLogo = document.querySelector("#light-mode-logo");
+const darkModeLogo = document.querySelector("#dark-mode-logo");
+const lightModeToggle = document.querySelector("#light-mode-toggle");
+const darkModeToggle = document.querySelector("#dark-mode-toggle");
+const heroSection = document.querySelector("#hero");
+const getStartbtn = document.querySelector("#get-start");
+const navbar = document.querySelector("#navbar");
+
+
+
+const projectStatus = document.querySelector(".project-status");
+const taskProgress = document.querySelector(".task-progress");
+
+
+themebtn.addEventListener("click", function () {
+
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+
+        projectStatus.src = "assets/dark-mode-project-status.png";
+        taskProgress.src = "assets/dark-mode-progress-task.png";
+        lightModeToggle.style.display = "block";
+        darkModeToggle.style.display = "none";
+        lightModeLogo.style.display = "none";
+        darkModeLogo.style.display = "block";
+
+    } else {
+        lightModeToggle.style.display = "none";
+        darkModeToggle.style.display = "block";
+        projectStatus.src = "assets/light-mode-project-status.png";
+        taskProgress.src = "assets/light-mode-progress-task.png";
+        lightModeLogo.style.display = "block";
+        darkModeLogo.style.display = "none";
+    }
+
+
+
+})
+
+
+
+
 const changetext = [
     "dynamic teams",
     "increased productivity",
@@ -42,7 +86,7 @@ const nameVerifyError = document.querySelector(".name-verify");
 fullName.addEventListener("input", function () {
     const nameRegex = /^[A-Za-z ]+$/;
     const name = fullName.value.trim();
-    
+
     if (name === "") {
         nameRequiredError.style.display = "flex"
         nameConditionError.style.display = "none"
@@ -89,7 +133,7 @@ email.addEventListener("input", function () {
         emailVerifyError.style.display = "none";
         email.style.borderColor = "green"
     }
-    
+
 })
 // Email Input Verification done
 
@@ -101,11 +145,11 @@ const passwordConditionError = document.querySelector(".password-condition-error
 const passwordVerifyError = document.querySelector(".password-verify");
 
 password.addEventListener("input", function () {
-    
+
     const passwordValue = password.value.trim();
-    
+
     const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
-    
+
     if (passwordValue === "") {
         passwordRequiredError.style.display = "flex";
         passwordConditionError.style.display = "none";
@@ -124,7 +168,7 @@ password.addEventListener("input", function () {
         passwordVerifyError.style.display = "none";
         password.style.borderColor = "green";
     }
-    
+
 });
 
 
@@ -138,7 +182,7 @@ const confirmPasswordConditionError = document.querySelector(".confirm-password-
 confirmPassword.addEventListener("input", function () {
     const confirmPasswordValue = confirmPassword.value.trim();
     const passwordValue = password.value.trim();
-    
+
     if (confirmPasswordValue === "") {
         confirmPasswordRequiredError.style.display = "flex";
         confirmPasswordConditionError.style.display = "none";
@@ -160,16 +204,16 @@ const signupPage = document.querySelector("#signup-page");
 
 signupbtn.addEventListener("click", function (eventdets) {
     eventdets.preventDefault();
-    
-    
+
+
     let isvalid = true;
     const nameRegex = /^[A-Za-z ]+$/;
     const name = fullName.value.trim();
-    
+
     nameRequiredError.style.display = "none";
     nameConditionError.style.display = "none";
     nameVerifyError.style.display = "none";
-    
+
     if (name === "") {
         nameRequiredError.style.display = "flex"
         fullName.style.borderColor = "red"
@@ -185,14 +229,14 @@ signupbtn.addEventListener("click", function (eventdets) {
     } else {
         fullName.style.borderColor = "green"
     }
-    
-    
+
+
     // full name validation done
-    
-    
+
+
     const emailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
     const emailValue = email.value.trim()
-    
+
     emailRequiredError.style.display = "none";
     emailConditionError.style.display = "none";
     emailVerifyError.style.display = "none";
@@ -212,16 +256,16 @@ signupbtn.addEventListener("click", function (eventdets) {
     } else {
         email.style.borderColor = "green"
     }
-    
+
     // email validation done
-    
+
     const passwordValue = password.value.trim();
     const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
-    
+
     passwordRequiredError.style.display = "none";
     passwordConditionError.style.display = "none";
     passwordVerifyError.style.display = "none";
-    
+
     if (passwordValue === "") {
         passwordRequiredError.style.display = "flex";
         password.style.borderColor = "red";
@@ -237,15 +281,15 @@ signupbtn.addEventListener("click", function (eventdets) {
     } else {
         password.style.borderColor = "green";
     }
-    
+
     // password validation done
-    
+
     const confirmPasswordValue = confirmPassword.value.trim();
     // const passwordValue = password.value.trim();
-    
+
     confirmPasswordRequiredError.style.display = "none";
     confirmPasswordConditionError.style.display = "none";
-    
+
     if (confirmPasswordValue === "") {
         confirmPasswordRequiredError.style.display = "flex";
         confirmPassword.style.borderColor = "red";
@@ -257,9 +301,9 @@ signupbtn.addEventListener("click", function (eventdets) {
     } else {
         confirmPassword.style.borderColor = "green";
     }
-    
+
     // confirm password validation done
-    
+
     if (!isvalid) {
         return;
     } else {
@@ -268,15 +312,17 @@ signupbtn.addEventListener("click", function (eventdets) {
             email: emailValue,
             password: passwordValue
         }
-        
+
         localStorage.setItem("user", JSON.stringify(userInfo));
-        
+
     }
-    
+
     signupPage.style.top = "100%";
     loginPage.style.top = "5%";
-    signupPage.style.display = "none";
-    
+    setTimeout(() => {
+        signupPage.style.display = "none";
+    }, 2000)
+
 })
 
 
@@ -288,29 +334,29 @@ const loginEmailErrors = document.querySelector("#login-email-error");
 
 loginEmail.addEventListener("input", function () {
     const userData = JSON.parse(localStorage.getItem("user"))
-    
+
     const emailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
     const emailValue = loginEmail.value.trim();
-    
+
     loginEmailRequiredErrors.style.display = "none"
     loginEmailConditionErrors.style.display = "none"
     loginEmailErrors.style.display = "none"
-    
-    
-    if(emailValue === "") {
+
+
+    if (emailValue === "") {
         loginEmailRequiredErrors.style.display = "flex"
         loginEmail.style.borderColor = "red"
-    }else if(!emailRegex.test(emailValue)){
+    } else if (!emailRegex.test(emailValue)) {
         loginEmailConditionErrors.style.display = "flex"
         loginEmail.style.borderColor = "red"
-    }else if(userData.email !== emailValue){
+    } else if (userData.email !== emailValue) {
         loginEmailErrors.style.display = "flex"
         loginEmail.style.borderColor = "red"
-    }else{
+    } else {
         loginEmail.style.borderColor = "green"
     }
-    
-    
+
+
 })
 
 // login email input verification done
@@ -323,24 +369,24 @@ const loginPasswordErrors = document.querySelector("#login-password-error");
 
 loginPassword.addEventListener("input", function () {
     const userData = JSON.parse(localStorage.getItem("user"))
-    
+
     const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     const passwordValue = loginPassword.value.trim();
-    
+
     loginpasswordRequiredErrors.style.display = "none"
     loginPasswordConditionErrors.style.display = "none"
     loginPasswordErrors.style.display = "none"
-    
-    if(passwordValue === ""){
+
+    if (passwordValue === "") {
         loginpasswordRequiredErrors.style.display = "flex"
         loginPassword.style.borderColor = "red"
-    }else if(userData.password !== passwordValue){
+    } else if (userData.password !== passwordValue) {
         loginPasswordErrors.style.display = "flex"
         loginPassword.style.borderColor = "red"
-    }else{
+    } else {
         loginPassword.style.borderColor = "green"
     }
-    
+
 })
 // login password input verification done
 
@@ -357,25 +403,25 @@ loginbtn.addEventListener("click", function (eventdets) {
 
     const emailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
     const emailValue = loginEmail.value.trim();
-    
+
     loginEmailRequiredErrors.style.display = "none"
     loginEmailConditionErrors.style.display = "none"
     loginEmailErrors.style.display = "none"
 
 
-    if(emailValue === "") {
+    if (emailValue === "") {
         loginEmailRequiredErrors.style.display = "flex"
         loginEmail.style.borderColor = "red"
         isvalid = false;
-    }else if(!emailRegex.test(emailValue)){
+    } else if (!emailRegex.test(emailValue)) {
         loginEmailConditionErrors.style.display = "flex"
         loginEmail.style.borderColor = "red"
         isvalid = false;
-    }else if(userData.email !== emailValue){
+    } else if (userData.email !== emailValue) {
         loginEmailErrors.style.display = "flex"
         loginEmail.style.borderColor = "red"
         isvalid = false;
-    }else{
+    } else {
         loginEmail.style.borderColor = "green"
     }
 
@@ -384,24 +430,24 @@ loginbtn.addEventListener("click", function (eventdets) {
 
     const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     const passwordValue = loginPassword.value.trim();
-    
+
     loginpasswordRequiredErrors.style.display = "none"
     loginPasswordConditionErrors.style.display = "none"
     loginPasswordErrors.style.display = "none"
 
-    if(passwordValue === ""){
+    if (passwordValue === "") {
         loginpasswordRequiredErrors.style.display = "flex"
         loginPassword.style.borderColor = "red"
         isvalid = false;
-    }else if(userData.password !== passwordValue){
+    } else if (userData.password !== passwordValue) {
         loginPasswordErrors.style.display = "flex"
         loginPassword.style.borderColor = "red"
         isvalid = false;
-    }else{
+    } else {
         loginPassword.style.borderColor = "green"
     }
 
-    if(isvalid){
+    if (isvalid) {
         loginPage.style.top = "105%";
         setTimeout(() => {
             loginPage.style.display = "none";
@@ -410,3 +456,4 @@ loginbtn.addEventListener("click", function (eventdets) {
     }
 
 })
+
